050b2c70a09ed2b4c76592188cfae68a9f21ba41
Bruno Panuto <bl.panuto@gmail.com>
2016-04-13 23:33:19 -0300

    added an api route to fetch the nearest hospitals

--- web/router.ex
+++ web/router.ex
@@ -14,13 +14,15 @@ defmodule Fluffy.Router do
   end
 
   scope "/", Fluffy do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Fluffy do
-  #   pipe_through :api
-  # end
+  scope "/api", Fluffy do
+    get "/", IndexController, :index
+
+    resources "/hospitals", HospitalController, except: [:new, :edit]
+    get "/nearest", IndexController, :nearest
+  end
 end
